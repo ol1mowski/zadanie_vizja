9d42908eb9c4df3ffc684f009e4ad17eced583e5
Oliwier <oliwier.markiewicz.dev@gmail.com>
2025-09-20 11:23:00 +0200

    feat: add footer component to the main application layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
+      <Footer />
     </div>
   );
 };
