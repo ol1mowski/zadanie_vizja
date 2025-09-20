3e2c494e363b0dae0a8411ec5780b2590eab146c
Oliwier <oliwier.markiewicz.dev@gmail.com>
2025-09-20 10:43:54 +0200

    feat: add spring boot starting files

---
+++ server/src/main/java/com/example/zadanieVizja/ZadanieVizjaApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadanieVizja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieVizjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieVizjaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/zadanieVizja/ZadanieVizjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanieVizja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieVizjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
