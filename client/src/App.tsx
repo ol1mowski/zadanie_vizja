1ebb393c93c63170def64e41204ebb83a558dd5b
Ol1mowski <olimarki123@wp.pl>
2025-09-22 10:24:33 +0200

    chore: update dependencies and refactor routing in Dashboard component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,20 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Dashboard } from './components/Dashboard';
 import { UserProvider } from './contexts/UserContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <UserProvider>
-      <div className="min-h-screen bg-gray-50">
-        <Header />
-        <main>
-          <Dashboard />
-        </main>
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-50">
+          <Header />
+          <main>
+            <Dashboard />
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </UserProvider>
   );
 };
--- client/src/components/Dashboard/Dashboard.component.tsx
+++ client/src/components/Dashboard/Dashboard.component.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
-import { UserType } from '../../types/user';
-import { useUser } from '../../contexts/UserContext';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { WelcomeComponent } from './components/WelcomeComponent.component';
 import { StudentDashboard } from './components/StudentDashboard.component';
 import { AdminDashboard } from './components/AdminDashboard.component';
 import { CandidateDashboard } from './components/CandidateDashboard.component';
 
 export const Dashboard: React.FC = () => {
-  const { userType } = useUser();
-  const renderDashboard = () => {
-    switch (userType) {
-      case UserType.UNAUTHENTICATED:
-        return <WelcomeComponent />;
-      case UserType.STUDENT:
-        return <StudentDashboard />;
-      case UserType.ADMIN:
-        return <AdminDashboard />;
-      case UserType.CANDIDATE:
-        return <CandidateDashboard />;
-      default:
-        return <WelcomeComponent />;
-    }
-  };
-
   return (
     <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       <div className="px-4 py-6 sm:px-0">
         <div className="bg-white overflow-hidden shadow rounded-lg">
           <div className="px-4 py-5 sm:p-6">
-            {renderDashboard()}
+            <Routes>
+              <Route path="/" element={<WelcomeComponent />} />
+              <Route path="/student" element={<StudentDashboard />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/candidate" element={<CandidateDashboard />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
           </div>
         </div>
       </div>
